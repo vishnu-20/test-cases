153eb53e5ab4e8571f4891f34a510642e57934c5
vishnu-20 <67593872+vishnu-20@users.noreply.github.com>
2024-10-22 20:39:42 +0530

    Update nithin.java

--- nithin.java
+++ nithin.java
@@ -54,3 +54,55 @@
 ObjectMapper objectMapper = new ObjectMapper();
     List<ActiveDirectoryUserModel> userModels;
         userModels = objectMapper.readValue(jsonData, new TypeReference<List<ActiveDirectoryUserModel>>() {});
+
+
+<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+<head>
+    <title>BulkUser Creation in AD</title>
+</head>
+<body>
+    <div layout:fragment="content">
+        <h2>Upload .XLSX file to Create Users in AD</h2>
+        <form id="uploadForm" th:action="@{/bulkUserCreationInAD}" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="file">File</label>
+                <input type="file" class="form-control-file" name="file" id="fileUpload" accept=".xlsx">
+                <small>Upload only .xlsx file</small>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+
+        <!-- Table for displaying the response -->
+        <h3>Result</h3>
+        <table id="resultTable" border="1" style="width: 100%;" th:if="${users}">
+            <thead>
+                <tr>
+                    <th>User Name</th>
+                    <th>Password</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Employee ID</th>
+                    <th>Environment Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Loop through the list of users and display in rows -->
+                <tr th:each="user : ${users}">
+                    <td th:text="${user.userName}"></td>
+                    <td th:text="${user.password}"></td>
+                    <td th:text="${user.firstName}"></td>
+                    <td th:text="${user.lastName}"></td>
+                    <td th:text="${user.email}"></td>
+                    <td th:text="${user.employeeId}"></td>
+                    <td th:text="${user.environmentName}"></td>
+                </tr>
+            </tbody>
+        </table>
+
+       
+    </div>
+</body>
+</html>
+
